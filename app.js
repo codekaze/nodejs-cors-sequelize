3e8f8120d9fefdd7d6299167c2ad6ecd0f5441ee
codekaze <codekaze.id@gmail.com>
2021-05-09 15:55:26 +0700

    ASyncronous

--- app.js
+++ app.js
@@ -24,25 +24,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Include All Modules in /module Dir
 var glob = require("glob")
-glob( 'module/**/*.js', function( err, files ) {
-  for(var i=0;i<files.length;i++){
-    var file = files[i].split(".").slice(0, -1).join(".");
-    console.log(file);
-    require("./" + file);
-  }
-});
+console.log("###################");
+var files = glob.sync('module/**/*.js');
+for (var i = 0; i < files.length; i++) {
+  var file = files[i].split(".").slice(0, -1).join(".");
+  console.log(file);
+  require("./" + file);
+}
+console.log("###################");
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
